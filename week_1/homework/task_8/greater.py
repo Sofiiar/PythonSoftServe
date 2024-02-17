e4b327fb43a6eeb0a95b34f461b347d0f61ca923
Sofiiar <s_radul-itssv@kw.com>
2024-02-17 06:12:56 +0100

    PY-1: check if number is greater

--- week_1/homework/task_8/greater.py
+++ week_1/homework/task_8/greater.py
@@ -6,4 +6,4 @@ try:
         y = sys.argv[2]
         print("y is greater than x" if y > x else "y is not greater than x")
 except ValueError:
-    print("You should type valid numbers.")
+    print("You should type into the console valid arguments. Example: python greater.py 4 5")
