b2270022c1d7dba494bd1c75401dc55e65bb3e7b
Sofiiar <s_radul-itssv@kw.com>
2024-02-22 05:01:07 +0100

    PY-2: sort numbers (task 10)

--- week_2/homework/task_10/sort_numbers.py
+++ week_2/homework/task_10/sort_numbers.py
@@ -1,5 +1,5 @@
-with open('numbers.txt', 'r') as f:
-    numbers = f.read()
-    numbers_raw_list = [int(number) for number in numbers.split(",")]
+with open('numbers.txt', 'r') as file:
+    raw_text = file.read()
+    numbers_raw_list = [int(number) for number in raw_text.split(",")]
     numbers_sorted_list = sorted(numbers_raw_list)
     print(numbers_sorted_list)
